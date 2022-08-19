91bafde487ca44c0de2879e82d1ced1307dc3907
Szymon Surówka <szymon121122@gmail.com>
2022-08-19 15:38:53 +0200

    function implemented

--- js/script.js
+++ js/script.js
@@ -1,12 +1,26 @@
-let buttonElement = document.querySelector(".js-button");
-let bodyElement = document.querySelector(".body");
-let themeNameElement = document.querySelector(".js-themeName");
-
-buttonElement.addEventListener("click", () => {
-    bodyElement.classList.toggle("dark");
-    if (bodyElement.classList.contains("dark")) {
-        themeNameElement.innerText = "jasny";
-    } else {
-        themeNameElement.innerText = "ciemny";
+{
+    const welcome = () => {
+        console.log("Witam wszystkich developerów");
     }
-});
+
+    const toggleBackground = () => {
+        const bodyElement = document.querySelector(".body");
+        const themeNameElement = document.querySelector(".js-themeName");
+
+        bodyElement.classList.toggle("dark");
+        if (bodyElement.classList.contains("dark")) {
+            themeNameElement.innerText = "jasny";
+        } else {
+            themeNameElement.innerText = "ciemny";
+        }
+    }
+
+    const init = () => {
+        const buttonElement = document.querySelector(".js-button");
+        buttonElement.addEventListener("click", toggleBackground);
+
+        welcome();
+    };
+
+init();
+}
